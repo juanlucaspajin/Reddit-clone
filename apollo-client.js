f3aa06c12a485dcad2d001f3e0bfbcdd8bc34e7f
Juan Lucas Pajin <lucas@psiware.com.ar>
2022-05-25 19:59:32 -0300

    Changing public key of stepzen

--- apollo-client.js
+++ apollo-client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
     uri: "https://honeybrook.stepzen.net/api/solid-pronghorn/__graphql",
     headers: {
-      Authorization: `Apikey ${process.env.STEPZEN_APIKEY}`
+      Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`
     },
     cache: new InMemoryCache(),
 });
